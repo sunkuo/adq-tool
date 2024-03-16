44bd3881891b4ea9402bf013fc2c4f37771c11a0
sun <sun@sundeMacBook-Pro.local>
2024-03-16 15:34:28 +0800

    打印 ip 地区

--- src/controller/click.controller.ts
+++ src/controller/click.controller.ts
@@ -1,10 +1,23 @@
-import { Controller, Get, Query } from "@midwayjs/core";
+import { Controller, Get, Query } from '@midwayjs/core';
+import axios from 'axios';
 
 @Controller('/')
 export class ClickController {
   @Get('/click')
-  async click(@Query('click_id') clickId: string): Promise<string> {
-    console.log('clickId:', clickId);
+  async click(
+    @Query('click_id') clickId: string,
+    @Query('ip') ip: string
+  ): Promise<string> {
+    if (!ip) return 'done';
+    const { data } = await axios.get('https://api.ip138.com/ip/', {
+      headers: {
+        token: '3d972ae9903b5111282f8c45b46eda10',
+      },
+      params: {
+        ip,
+      },
+    });
+    console.log('clickId:', clickId, 'ip: ', ip, 'region: ', data.data);
     return 'done';
   }
 }
